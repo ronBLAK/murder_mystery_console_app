f6329b52909df4c89e2c3e1f3231dd4d11ff9ff5
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2024-09-22 17:55:51 +1200

    Update MurderMysteries.py

--- MurderMysteries.py
+++ MurderMysteries.py
@@ -1,7 +1,8 @@
 # murder mystery solve game
 import random
+from save_case import SaveCaseInfo
 
-class case_gen:
+class CaseGen:
     culprit_name_list = ["Jerome", "Max", "Zach", "Tom", "Carl", "Tim", "Cooper", "Shaw", "Mat"] # holds the different possible names for the culprit
     victim_name_list = ['Sten', 'Troy', 'Dan'] # holds the different possible names for the victim
     murder_weapon_list = ['knife', 'gun', 'baseball bat', 'water'] # holds the different possible murder weapons
@@ -36,6 +37,7 @@ class case_gen:
         return "Detective " + formatted_name
 
     # generates a random case with random components passed into the case function. this is stored in a json file for comparison with the detective's criminal
+    @staticmethod
     def generate_case(culprit_name, murder_weapon, murder_location, murder_type, weather, time_of_day, is_witness_present, witness_num, victim_name, has_injury, injury_location, suspects):
         # holds all the values that need to be save to the newly created, locked json file
         case_components = {
@@ -52,52 +54,54 @@ class case_gen:
             'injury location': injury_location,
             'selected suspects': suspects
         }
-        
-        print(case_components)
+        SaveCaseInfo.save_case(case_components)
         return case_components
     
     # generates a case file from the same content of the case method. this is the story version that the detective sees
+    @staticmethod
     def generate_case_file(victim_name, murder_weapon, has_injury, injury_location, murder_location, murder_type, weather, time_of_day, witness_num, num_suspects, suspects):
         if has_injury == True:
             case_file = f'{victim_name} was found dead in a {murder_location}. The cause of death\nwas a {murder_weapon} {murder_type} wound on the {injury_location}. It was\na {weather} {time_of_day}, and there is {witness_num} witnesses to this\ncrime. There are {len(num_suspects)} suspects to this crime. They are\n{suspects}'
-            print(case_file)
         else:
             case_file = f'{victim_name} was found dead in a {murder_location}. The cause of death\nwas {murder_type}ing. There were no injuries on the body according\nto autopsy reports. It was a {weather} {time_of_day} and\nthere is {witness_num} witnesses to this crime. There are {len(num_suspects)} suspects\nto this crime. They are {suspects}'
-            print(case_file)
+        
+        SaveCaseInfo.save_case_file(case_file)
+        print(case_file)
+        return case_file
 
     # determines the name of the culprit
-    def culprit_name(possible_culprit_names):
+    def get_culprit_name(possible_culprit_names):
         culprit_name = random.choice(possible_culprit_names)
         
         return culprit_name
 
     # determines the name of the victim
-    def victim_name(possible_victim_names):
+    def get_victim_name(possible_victim_names):
         victim_name = random.choice(possible_victim_names)
         
         return victim_name
 
     # determines the location of the murder
-    def murder_location(possible_murder_locations):
+    def get_murder_location(possible_murder_locations):
         murder_location = random.choice(possible_murder_locations)
         
         return murder_location
 
     # determines the weapon used for the murder
-    def murder_weapon(possible_murder_weapons):
+    def get_murder_weapon(possible_murder_weapons):
         murder_weapon = random.choice(possible_murder_weapons)
         
         return murder_weapon
 
     # determines how to murder was executed (were they stabbed? drowned? etc)
-    def murder_type(weapon, possible_murder_types):
+    def get_murder_type(weapon, possible_murder_types):
         if weapon in possible_murder_types:
             murder_type = random.choice(possible_murder_types[weapon])
             
         return murder_type
 
     # determines if the victim had an injury on the body in the crime scene
-    def injury_location(murder_type, possible_injury_types):
+    def get_injury_location(murder_type, possible_injury_types):
         if murder_type in possible_injury_types:
             injury_location = random.choice(possible_injury_types[murder_type])
             
@@ -113,13 +117,13 @@ class case_gen:
             return False
 
     # determines the weather of the day of incident
-    def weather(possible_weathers):
+    def get_weather(possible_weathers):
         weather = random.choice(possible_weathers)
         
         return weather
 
     # determines the time of the day
-    def time_of_day(possible_times_of_day):
+    def get_time_of_day(possible_times_of_day):
         time_of_day = random.choice(possible_times_of_day)
         
         return time_of_day
@@ -131,7 +135,7 @@ class case_gen:
         return state_is_witness_present
 
     # if yes, determines the number of witnesses
-    def number_of_witnesses(is_witness_present):
+    def get_number_of_witnesses(is_witness_present):
         witness_present = is_witness_present
         
         if witness_present:
@@ -142,7 +146,7 @@ class case_gen:
         return str(number_of_witnesses), witness_present
 
     # returns a list of 4 suspects including 3 random suspects and the returned culprit himself
-    def suspects(culprit, culprit_name_list):
+    def get_suspects(culprit, culprit_name_list):
         selected_suspects = [None] * 4 # creates an empty list of length 4 to be populated with the suspects of the case
         culprit_index_in_suspects_list = random.randint(0, 3) # generates a random number from within the length of the selected suspects to place the culprit in (the culprit name position is randomized so that the culprit name does not always show up at the same spot)
         suspects = culprit_name_list.copy() # creates a copy of the culprits name list to act as the suspect pool for the case
@@ -159,35 +163,22 @@ class case_gen:
         return selected_suspects
 
     # sets all the returned components of the case into their own variables so that the same instance of each function can be used across the different types (case and case file)
-    selected_culprit_name = culprit_name(culprit_name_list)
-    selected_murder_location = murder_location(murder_location_list)
-    selected_weapon = murder_weapon(murder_weapon_list)
-    selected_murder_type = murder_type(selected_weapon, murder_type_dict)
-    selected_injury_location = injury_location(selected_murder_type, injury_location_dict)
-    selected_time_of_day = time_of_day(time_of_day_list)
-    selected_weather = weather(weather_list)
-    selected_number_of_witness, selected_is_witness_present = number_of_witnesses(is_witness_present())
-    selected_victim_name = victim_name(victim_name_list)
+    selected_culprit_name = get_culprit_name(culprit_name_list)
+    selected_murder_location = get_murder_location(murder_location_list)
+    selected_weapon = get_murder_weapon(murder_weapon_list)
+    selected_murder_type = get_murder_type(selected_weapon, murder_type_dict)
+    selected_injury_location = get_injury_location(selected_murder_type, injury_location_dict)
+    selected_time_of_day = get_time_of_day(time_of_day_list)
+    selected_weather = get_weather(weather_list)
+    selected_number_of_witness, selected_is_witness_present = get_number_of_witnesses(is_witness_present())
+    selected_victim_name = get_victim_name(victim_name_list)
     selected_has_injuries = has_injuries(selected_injury_location)
     
-    selected_suspects_list = suspects(selected_culprit_name, culprit_name_list)
+    selected_suspects_list = get_suspects(selected_culprit_name, culprit_name_list)
     formatted_selected_suspects_list = ', '.join(selected_suspects_list)
-
-    # testing for each of the separate components of the case
-
-    # print(selected_culprit_name)
-    # print(selected_murder_location)
-    # print(selected_weapon)
-    # print(selected_murder_type)
-    # print(selected_injury_location)
-    # print(selected_has_injuries)
-    # print(selected_weather)
-    # print(selected_time_of_day)
-    # print(selected_is_witness_present)
-    # print(selected_number_of_witness)
-    # print(selected_suspects_list)
     
-
+    # calls all the functions that work to generate the case and case file
     generate_case(selected_culprit_name, selected_weapon, selected_murder_location, selected_murder_type, selected_weather, selected_time_of_day, selected_is_witness_present, selected_number_of_witness, selected_victim_name, selected_has_injuries, selected_injury_location, selected_suspects_list)
-    print("")
+    
     generate_case_file(selected_victim_name, selected_weapon, selected_has_injuries, selected_injury_location, selected_murder_location, selected_murder_type, selected_weather, selected_time_of_day, selected_number_of_witness, selected_suspects_list, formatted_selected_suspects_list)
+    
