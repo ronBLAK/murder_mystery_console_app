b3d5cd028760b4ec019fb93ce66cfb3340f3b2df
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2024-09-25 17:37:08 +1200

    Update case_gen.py

--- case_gen.py
+++ case_gen.py
@@ -1,6 +1,7 @@
 # murder mystery solve game
 import random
 from save import Save
+import json
 
 class CaseGen:
     culprit_name_list = ["Jerome", "Max", "Zach", "Tom", "Carl", "Tim", "Cooper", "Shaw", "Mat"] # holds the different possible names for the culprit
@@ -153,3 +154,45 @@ class CaseGen:
                 
         # print(culprit_index_in_suspects_list)
         return selected_suspects
+    
+    def generate_case_and_case_file_random():
+        case_data_save_file = 'case data.json' # stores the file name that needs to be opened into a variable
+        
+        print('')
+        
+        # set the values returned in each of the separate case component funciton to its own variable
+        selected_culprit_name = CaseGen.get_culprit_name(CaseGen.culprit_name_list)
+        selected_murder_location = CaseGen.get_murder_location(CaseGen.murder_location_list)
+        selected_weapon = CaseGen.get_murder_weapon(CaseGen.murder_weapon_list)
+        selected_murder_type = CaseGen.get_murder_type(selected_weapon, CaseGen.murder_type_dict)
+        selected_injury_location = CaseGen.get_injury_location(selected_murder_type, CaseGen.injury_location_dict)
+        selected_time_of_day = CaseGen.get_time_of_day(CaseGen.time_of_day_list)
+        selected_weather = CaseGen.get_weather(CaseGen.weather_list)
+        selected_number_of_witness, selected_is_witness_present = CaseGen.get_number_of_witnesses(CaseGen.is_witness_present())
+        selected_victim_name = CaseGen.get_victim_name(CaseGen.victim_name_list)
+        selected_has_injuries = CaseGen.has_injuries(selected_injury_location)
+        selected_suspects_list = CaseGen.get_suspects(selected_culprit_name, CaseGen.culprit_name_list)
+
+        # calls all the functions that work to generate the case and case file
+        CaseGen.generate_case(selected_culprit_name, selected_weapon, selected_murder_location, selected_murder_type, selected_weather, selected_time_of_day, selected_is_witness_present, selected_number_of_witness, selected_victim_name, selected_has_injuries, selected_injury_location, selected_suspects_list)
+
+        # attempts to pull data from the case data save file and set it equal to new variables to be used to generate a case file for the generated case 
+        with open(case_data_save_file, 'r') as json_file:
+            case_data = json.load(json_file)
+
+        pulled_culprit_name = case_data.get('culprit name')
+        pulled_murder_weapon = case_data.get('murder weapon')
+        pulled_murder_location = case_data.get('murder location')
+        pulled_murder_type = case_data.get('murder type')
+        pulled_weather = case_data.get('weather')
+        pulled_time_of_day = case_data.get('time of day')
+        pulled_witness_number = case_data.get('witness number')
+        pulled_victim_name = case_data.get('victim name')
+        pulled_has_injury = case_data.get('has injury')
+        pulled_injury_location = case_data.get('injury location')
+        pulled_suspects_list = case_data.get('selected suspects')
+        formatted_suspects_list = ', '.join(pulled_suspects_list)
+
+        CaseGen.generate_case_file(pulled_victim_name, pulled_murder_weapon, pulled_has_injury, pulled_injury_location, pulled_murder_location, pulled_murder_type, pulled_weather, pulled_time_of_day, pulled_witness_number, pulled_suspects_list, formatted_suspects_list)
+        print('')
+        return pulled_culprit_name
