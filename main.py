ec8077c84ba5869858e8b0e279f60e6ae26f62c9
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2024-09-26 16:35:20 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from case_gen import CaseGen
 from detective_profile import DetectiveProfile
 import detective_attributes
 import time
+import os
 
 # gets and stores all information of detective from save file for detective and assigns it to variables to be used in main.py
 fame = detective_attributes.fame
 cases_solved = detective_attributes.cases_solved
+
+# stores the names of different save file into variables
+case_data = 'case data.json'
+case_file = 'case file.json'
+detective_data = 'detective data.json'
 
 # whether the user accpets or declines the case
 def question_start_solve():
@@ -14,7 +20,7 @@ def question_start_solve():
     print('')
     start_solve = input('enter your choice here --> ')
     print('')
-    
+
     return start_solve
 
 # function to display the commands that the user can choose
@@ -28,7 +34,7 @@ def commands():
     print('6. exit')
     print('')
 
-# function to store criteria for each action that the detective can carry out while attempting to solve the case
+# defines all actions available to detective in inverstigator menu, and their criteria
 def user_menu_interaction(user_choice):
     if user_choice == '1':
         print('you can view the profile of your existing detective account in this menu')
@@ -50,33 +56,65 @@ print("")
 
 # asks the user for their detective name
 detective_name = DetectiveProfile.detective_name()
+print('')
 
-# checks if the detective name entered is already present in the dictionary
-# if name present in dictionary, does not call the save function, and shows the previously saved name in dictionary. also displays previously saved case, as this is should be the case that is affiliated with the previous detective
-if detective_name == DetectiveProfile.read_detective_info():
-    print(f'Hello {detective_name}. Lets pick up your case right where you left it off')
-    DetectiveProfile.get_detective_info_as_dict(detective_name, fame, cases_solved,save = False)
-    print('')
-    print(CaseGen.read_case_file())
-    print('')
-    print('opening investigator menu...')
-    print('')
-    time.sleep(1)
+if os.path.exists(case_file) and os.path.exists(case_data) and os.path.exists(detective_data):
+    if detective_name == DetectiveProfile.read_detective_info():
+        print(f'hello {detective_name}. Lets pick up your case right where you left it off.')
+        print('')
+        print(CaseGen.read_case_file())
+        print('')
+        print('opening investigator menu...')
+        print('')
+        time.sleep(1)
 
-    print('the case is all yours, detective...')
-    commands()
-    print('')
+        print('the case is all yours, detective...')
+        commands()
+        print('')
 
-    while True:
-        user_menu_choice = input('enter your choice here --> ')
+        while True:
+            user_menu_choice = input('enter your choice here --> ')
 
-        if user_menu_choice == '6':
-            user_menu_interaction(user_menu_choice)
-            break
-        else:
-            user_menu_interaction(user_menu_choice)
-            print('')
-# if not present in dictionary, proceeds to save the new detective info into dictionary, and generates a new case
+            if user_menu_choice == '6':
+                user_menu_interaction(user_menu_choice)
+                break
+            else:
+                user_menu_interaction(user_menu_choice)
+                print('')
+    else:
+        DetectiveProfile.get_detective_info_as_dict(detective_name, fame, cases_solved)
+        print('')
+
+        print(f'Hello {detective_name}, you have a case waiting for you.')
+        print('')
+
+        # generates random case, and random case file according to information from the case
+        CaseGen.generate_case_and_case_file_random()
+
+        # checks and validates the input
+        while question_start_solve() != '1':
+            print('New Case:')
+            CaseGen.generate_case_and_case_file_random()
+
+        # this is what happens if the user selects the given case instead of generating a new case
+        print('case selected')
+        print('opening investigator menu...')
+        print('')
+        time.sleep(1)
+
+        print('the case is all yours, detective...')
+        commands()
+        print('')
+
+        while True:
+            user_menu_choice = input('enter your choice here --> ')
+
+            if user_menu_choice == '6':
+                user_menu_interaction(user_menu_choice)
+                break
+            else:
+                user_menu_interaction(user_menu_choice)
+                print('')
 else:
     DetectiveProfile.get_detective_info_as_dict(detective_name, fame, cases_solved)
     print('')
